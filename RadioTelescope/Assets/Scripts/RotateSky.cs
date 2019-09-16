1560757a8554f449f3d8eb9da28f2442303be941
Greg Plachno <gplachno@ycp.edu>
2019-09-16 15:26:53 -0400

    Upload Project

---
+++ RadioTelescope/Assets/Scripts/RotateSky.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    // Update is called once per frame
+    public float rotateSpeed = 1.1f;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed);
+    }
+}
