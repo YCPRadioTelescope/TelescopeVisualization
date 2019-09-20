c21c5394cb9357d46e802b0583464e519012fb27
Greg Plachno <gplachno@ycp.edu>
2019-09-20 15:24:05 -0400

    Telescope can move

---
+++ RadioTelescope/Assets/Scripts/MoveTelescope.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTelescope : MonoBehaviour
+{
+    public GameObject telescope;
+
+    public TelescopeController tc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tc = telescope.GetComponent<TelescopeController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            tc.RotateX(0.5f);
+        }
+        
+        if (Input.GetKey("down"))
+        {
+            tc.RotateX(-0.5f);
+        }
+        
+        if (Input.GetKey("right"))
+        {
+            tc.RotateY(0.5f);
+        }
+        
+        if (Input.GetKey("left"))
+        {
+            tc.RotateY(-0.5f);
+        }
+    }
+}
---
+++ RadioTelescope/Assets/Scripts/TelescopeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelescopeController : MonoBehaviour
+{
+    public GameObject xRotation;
+    public GameObject yRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(RotateX(1));
+       // Debug.Log(RotateY(0.2f));
+    }
+
+    public float RotateX(float speed)
+    {
+        xRotation.transform.Rotate(speed,0,0);
+
+        return xRotation.transform.rotation.x;
+        
+    }
+    
+   public float RotateY(float speed)
+    {
+        yRotation.transform.Rotate(0,speed,0);
+
+        return yRotation.transform.rotation.y;
+
+    }
+}
