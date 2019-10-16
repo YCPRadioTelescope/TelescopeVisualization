60c796a52aa36a694156c3a57551af161549bdc5
Greg Plachno <gplachno@ycp.edu>
2019-10-16 14:08:07 -0400

    updates

---
+++ RadioTelescope/Assets/Scripts/Sim.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Sim : MonoBehaviour
+{
+    
+    void Start()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
