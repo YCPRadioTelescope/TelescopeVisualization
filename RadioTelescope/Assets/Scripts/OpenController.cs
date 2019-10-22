8f3b8259d8c43e257f4d5447f85859fd47ac9621
Greg Plachno <gplachno@ycp.edu>
2019-10-22 19:46:55 -0400

    implement VR controls for telescope

---
+++ RadioTelescope/Assets/Scripts/OpenController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class OpenController : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
