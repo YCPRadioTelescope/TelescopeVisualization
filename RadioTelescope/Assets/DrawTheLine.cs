388798b37412f54a83ea8f1c877674afbfbc4209
Greg Plachno <gplachno@ycp.edu>
2019-11-11 14:08:39 -0500

    Trigger Pointer Added

---
+++ RadioTelescope/Assets/DrawTheLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DrawTheLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private LineRenderer lr;
+    public GameObject end;
+    void Start()
+    {
+        lr = this.transform.GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = this.transform.position;
+        newPos.y = newPos.y + 0.05f;
+        lr.SetPosition(0, newPos);
+        lr.SetPosition(1, end.transform.position);
+        //lr.SetPosition(0, (this.transform.forward.normalized * 1000));
+    }
+}
---
+++ RadioTelescope/Assets/Scripts/TriggerValue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.CameraRig.UnityXRCameraRig.Input;
+
+public class TriggerValue : MonoBehaviour
+{
+    public UnityAxis1DAction rightTrigger;
+    public GameObject rightLine;
+
+    public GameObject controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Debug.Log(rightTrigger.Value);
+        if (rightTrigger.Value > 0.2f)
+        {
+            rightLine.SetActive(true);
+        }
+        else
+        {
+            rightLine.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void OnDrawGizmos()
+    {
+       
+        
+    }
+}
