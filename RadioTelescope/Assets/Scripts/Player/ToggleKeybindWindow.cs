85e1b8ca3a6abd767313a1838cc3b0154bfbb69e
JmyerYCP <jmyer@ycp.edu>
2020-03-11 11:12:53 -0400

    Added keybinding window

---
+++ RadioTelescope/Assets/Scripts/Player/ToggleKeybindWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleKeybindWindow : MonoBehaviour
+{
+    public GameObject KeybindCollapse;
+    public GameObject KeybindExpand;
+    private bool toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            toggle = !toggle;
+        }
+            if (toggle)
+        {
+            KeybindCollapse.SetActive(false);
+            KeybindExpand.SetActive(true);
+        } else
+        {
+            KeybindCollapse.SetActive(true);
+            KeybindExpand.SetActive(false);
+        }
+    }
+}
