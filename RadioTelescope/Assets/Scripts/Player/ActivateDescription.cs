899de3caae24a6086b2e398aa24fd70b47559115
YCPjsteck <jsteck@ycp.edu>
2020-10-31 15:14:54 -0400

    fix: ActivateDescription is now always disabled on game start
    Previously it could be the case that ActivateDescription was active on game start, but this should always be deactivated when first starting.

--- RadioTelescope/Assets/Scripts/Player/ActivateDescription.cs
+++ RadioTelescope/Assets/Scripts/Player/ActivateDescription.cs
@@ -2,44 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script controls the activation of the HightlightTarget object,
+// which highlights the part of the radio telescope that is clicked on
+// and shows its name and description.
 public class ActivateDescription : MonoBehaviour
 {
-    public GameObject highlight;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            highlight.SetActive(true);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            highlight.SetActive(false);
-        }
-    }
-
-    private void OnEnable()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            highlight.SetActive(true);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            highlight.SetActive(false);
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            highlight.SetActive(true);
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            highlight.SetActive(false);
-        }
-    }
+	public GameObject highlight;
+	
+	// Start is called before the first frame update.
+	void Start()
+	{
+		// Ensure that the highlighter is inactive at game start.
+		highlight.SetActive(false);
+	}
+	
+	// Update is called once per frame.
+	void Update()
+	{
+		// Check if the mouse button is being held. If so, activate
+		// the hightlighted game object, which causes its name and
+		// description to be shown..
+		if(Input.GetMouseButtonDown(0))
+			highlight.SetActive(true);
+		else if(Input.GetMouseButtonUp(0))
+			highlight.SetActive(false);
+	}
 }
