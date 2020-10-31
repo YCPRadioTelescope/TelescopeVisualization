3e8010db419d1359a25b4917fd0dccb047cd9658
YCPjsteck <jsteck@ycp.edu>
2020-10-31 15:20:33 -0400

    feat: Rotate the stars in the sky about the Earth's axis

--- RadioTelescope/Assets/Scripts/Starfield.cs
+++ RadioTelescope/Assets/Scripts/Starfield.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script controls the creation and movement of the stars in the sky.
 public class Starfield : MonoBehaviour
 {
 	public ParticleSystem particleSystem;
-	public int maxParticles = 1000;
+	public int maxParticles;
 	public TextAsset starCSV;
+	private Vector3 rotationalAxis;
 	
+	// Awake is called on the initalization of all game objects.
 	void Awake()
 	{
+		// Schedule a burst of maxParticles particles so that all
+		// the stars are created at once.
 		particleSystem = GetComponent<ParticleSystem>();
 		var main = particleSystem.main;
 		main.maxParticles = maxParticles;
@@ -21,114 +26,84 @@ public class Starfield : MonoBehaviour
 		particleSystem.emission.SetBursts(bursts, 1);
 	}
 	
-	// Start is called before the first frame update
+	// Start is called before the first frame update.
 	void Start()
 	{
+		// Rotate the particleSystem about the global axis, simulating the latitude of the observer.
+		// TODO: Accurately rotate the particleSystem to position the stars as they are in person.
+		// TODO: Figure out the angles to rotate the particleSystem by given the time of year.
+		// Testing with a random tilt right now.
+		particleSystem.transform.Rotate(0.0f, 23.0f, 0.0f, Space.World);
+		rotationalAxis = new Vector3(0.0f, 1.0f, 0.0f);
 	}
 	
-	// Update is called once per frame
+	// Update is called once per frame.
 	void Update()
 	{
+		// Rotate the sky about the Earth's axis at the given rotational speed.
+		// This rotates the particleSystem relative to itself.
+		particleSystem.transform.Rotate(rotationalAxis, RotationalSpeed(24.0f, 60.0f), Space.Self);
 	}
 	
+	// A function to compute the angle per frame necessary to cause the
+	// given number of hours to elapse in the given number of seconds.
+	float RotationalSpeed(float hours, float seconds)
+	{
+		return 360.0f / (24.0f / hours) / seconds / 60.0f;
+	}
+	
+	// LastUpdate is called once per frame after every Update function has been called.
 	void LateUpdate()
-	{	
+	{
+		// Split the starCSV file into each line.
 		string[] lines = starCSV.text.Split('\n');
+		// Create maxParticles particles and initalize them.
 		ParticleSystem.Particle[] particleStars = new ParticleSystem.Particle[maxParticles];
 		particleSystem.GetParticles(particleStars);
 		for(int i = 0; i < maxParticles; i++)
 		{
+			// Split each line by the commas.
 			string[] components = lines[i].Split(',');
-			// Color.white * (1.0f – ((float.Parse(components[0]) + 1.44f) / 8));
-			// 0 magnitude = 100x a 5 magnitude
-			// 0 vs 1 = 2.5
-			// Logarithmic
-			// 0 = 255
-			// 1 = 112
-			// 2 = 42
-			// 3 = 16
-			// 4 = 7
-			// Do a grid in the sky for right ascension and declination
-			// Pie wedges going to the north star and south pole to give you a sense of direction
+			
+			// The first component of each line is the magnitude.
 			float mag = float.Parse(components[0]);
-			int mode = -3;
-			// Make the stars twinkle (random +/- brightness)
-			// Requirement: must be logarithmic
-			if(mode == -3)
+			
+			// 
+			int mode = 0;
+			if(mode == 0)
 			{
-				// Dynamic, 2 scaling
-				if(mag < 0)
-					mag = 0;
-				mag = (float)Math.Pow(2, mag);
-				particleStars[i].startColor = new Color(1, 1, 1, 1 / mag);
-			}
-			else if(mode == -2)
-			{
-				// 5 buckets, sqrt(2) scaling
+				// Static 5 bucket brightness with log base 2 scaling.
 				if(mag < 1.5)
 					particleStars[i].startColor = new Color32(255, 255, 255, 255);
 				else if(mag < 2.5)
 					particleStars[i].startColor = new Color32(255, 255, 255, 128);
 				else if(mag < 3.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 90);
-				else if(mag < 4.5)
 					particleStars[i].startColor = new Color32(255, 255, 255, 64);
+				else if(mag < 4.5)
+					particleStars[i].startColor = new Color32(255, 255, 255, 32);
 				else
-					particleStars[i].startColor = new Color32(255, 255, 255, 45);
-			}
-			else if(mode == -1)
-			{
-				// Dynamic, sqrt(2) scaling
-				if(mag < 0)
-					mag = 0;
-				mag = (float)Math.Pow(Math.Sqrt(2), mag);
-				particleStars[i].startColor = new Color(1, 1, 1, 1 / mag);
-			}
-			else if(mode == 0)
-			{
-				// Dynamic, 1/mag scaling
-				if(mag < 1)
-					mag = 1;
-				if(mag > 5)
-					mag = 5;
-				particleStars[i].startColor = new Color(1, 1, 1, 1 / mag);
-			}
-			else if(mode == 1)
-			{
-				// Darkest stars half max
-				if(mag < 0.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 255);
-				else if(mag < 1.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 212);
-				else if(mag < 2.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 177);
-				else if(mag < 3.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 146);
-				else if(mag >= 3.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 123);
-			}
-			else if(mode == 2)
-			{
-				// Original suggestion; 100^(1/5) scaling(?)
-				if(mag < 0.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 255);
-				else if(mag < 1.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 112);
-				else if(mag < 2.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 42);
-				else if(mag < 3.5)
 					particleStars[i].startColor = new Color32(255, 255, 255, 16);
-				else if(mag >= 3.5)
-					particleStars[i].startColor = new Color32(255, 255, 255, 7);
 			}
 			else
-				// No scaling
-				particleStars[i].startColor = new Color32(255, 255, 255, 255);
+			{
+				// Dynamic brightness with log base 2 scaling.
+				if(mag < 0)
+					mag = 0;
+				mag = (float)Math.Pow(2, mag);
+				particleStars[i].startColor = new Color(1, 1, 1, 1 / mag);
+			}
+			// Create a vector using the X, Y, and Z coordinates of the star.
+			// Compontents 2 and 3 are intentionally switched.
 			Vector3 starPosition = new Vector3( float.Parse(components[1]),
 												float.Parse(components[3]),
 												float.Parse(components[2]));
+			
+			// Normalize the vector to a length of one, then move it out to
+			// 900 units from the particle system. This puts the stars just
+			// before the far clipping plane of the camera.
 			particleStars[i].position = Vector3.Normalize(starPosition) * 900;
-			particleStars[i].remainingLifetime = Mathf.Infinity;
+			// Particles only exist for one frame before a new one is created.
+			particleStars[i].remainingLifetime = 1;
 		}
 		particleSystem.SetParticles(particleStars, maxParticles);
 	}
