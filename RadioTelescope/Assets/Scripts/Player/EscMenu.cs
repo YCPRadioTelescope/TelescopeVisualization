db178d297bedbd9f1dce65bb2e41b933d0fbba47
YCPjsteck <jsteck@ycp.edu>
2020-10-31 15:16:59 -0400

    fix: Escape menu buttons now work
    The click listeners were not being activated when the escape menu was opened after the first time it was opened.
    
    The escape menu also no longer starts activated.

--- RadioTelescope/Assets/Scripts/Player/EscMenu.cs
+++ RadioTelescope/Assets/Scripts/Player/EscMenu.cs
@@ -5,44 +5,48 @@ using UnityEngine.UI;
 
 public class EscMenu : MonoBehaviour
 {
-    private bool pause = false;
-    public GameObject pauseMenu;
-    public Button resumeButton;
-    public Button exitButton;
-    public mouseLook mouseLook;
-
-    private void Start()
-    {
-        pauseMenu.SetActive(true);
-        resumeButton.onClick.AddListener(setPauseFalse);
-        exitButton.onClick.AddListener(Application.Quit);
-        pauseMenu.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Cancel"))
-        {
-            pause = !pause;
-        }
-        if (pause == true)
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            mouseLook.enabled = false;
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            mouseLook.enabled = true;
-        }
-        pauseMenu.SetActive(pause);
-    }
-    private void setPauseFalse()
-    {
-        pause = false;
-        Debug.Log("set pause false");
-    }
+	private bool pause = false;
+	public GameObject pauseMenu;
+	public Button resumeButton;
+	public Button exitButton;
+	public mouseLook mouseLook;
+	
+	// Start is called before the first frame update.
+	void Start()
+	{
+		// Don't show the pause menu when the game is started.
+		pauseMenu.SetActive(false);
+	}
+	
+	// Update is called once per frame.
+	void Update()
+	{
+		// If escape is pressed, toggle the state of the pause menu.
+		if(Input.GetButtonDown("Cancel"))
+			pause = !pause;
+		
+		if(pause)
+		{
+			// If pause is active, unlock the cursor and add on click 
+			// listeners to see if the resume or exit buttons are pressed.
+			Cursor.lockState = CursorLockMode.None;
+			resumeButton.onClick.AddListener(setPauseFalse);
+			exitButton.onClick.AddListener(Application.Quit);
+			Cursor.visible = true;
+			mouseLook.enabled = false;
+		}
+		else
+		{
+			// If pause is inactive, clock the mouse to the player view.
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+			mouseLook.enabled = true;
+		}
+		pauseMenu.SetActive(pause);
+	}
+	
+	private void setPauseFalse()
+	{
+		pause = false;
+	}
 }
