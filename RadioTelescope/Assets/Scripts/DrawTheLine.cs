0031118c0f1559c0ce4eab29213fd5c0efdefccd
YCPjsteck <jsteck@ycp.edu>
2020-10-31 18:41:42 -0400

    housekeeping: Forgot the check these two files for the last commit
    Also added nullptr checking that the console was warning about.

--- RadioTelescope/Assets/Scripts/DrawTheLine.cs
+++ RadioTelescope/Assets/Scripts/DrawTheLine.cs
@@ -7,81 +7,83 @@ using UnityEngine.UI;
 
 public class DrawTheLine : MonoBehaviour
 {
-    private LineRenderer lr;
-    public GameObject end;
-    public GameObject start;
-    private RaycastHit hitInfo;
-    private Material origMat, tempMat;
-    public Color highlightColor;
-    public Shader shader;
-    private Renderer rend = null;
-    private Renderer currRend;
-    public Text text;
+	public GameObject start;
+	public GameObject end;
+	public Color highlightColor;
+	public Shader shader;
+	public Text text;
 	
-    // Start is called before the first frame update
-    void Start()
-    {
-        lr = this.transform.GetComponent<LineRenderer>();
-    }
+	private LineRenderer lr;
+	private RaycastHit hitInfo;
+	private Material origMat, tempMat;
+	private Renderer rend = null;
+	private Renderer currRend;
+	
+	// Start is called before the first frame update
+	void Start()
+	{
+		lr = this.transform.GetComponent<LineRenderer>();
+	}
 
-    private void OnEnable()
-    {
-        lr.SetPosition(0, start.transform.position);
-        lr.SetPosition(1, end.transform.position);;
-    }
+	private void OnEnable()
+	{
+		lr.SetPosition(0, start.transform.position);
+		lr.SetPosition(1, end.transform.position);;
+	}
 
-    private void OnDisable()
-    {
-        rend.sharedMaterial = origMat;
-        rend = null;
-        text.text = "";
-    }
+	private void OnDisable()
+	{
+		if(rend)
+			rend.sharedMaterial = origMat;
+		rend = null;
+		text.text = "";
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        var dir = start.transform.forward * -10000;
-        lr.SetPosition(0, start.transform.position);
-        lr.SetPosition(1, end.transform.position);
-        if (Physics.Raycast(start.transform.position, dir, out hitInfo, Vector3.Distance(start.transform.position, end.transform.position)))
-        {
-            Debug.Log("we Hit this: " +  hitInfo.transform.GetComponent<ObjectDesc>().Name);
-            text.text = hitInfo.transform.GetComponent<ObjectDesc>().Name + ": " + hitInfo.transform.GetComponent<ObjectDesc>().Description;
-            Debug.DrawRay(start.transform.position, dir);
-            
-            currRend = hitInfo.collider.gameObject.GetComponent<Renderer>();
+	// Update is called once per frame
+	void Update()
+	{
+		var dir = start.transform.forward * -10000;
+		lr.SetPosition(0, start.transform.position);
+		lr.SetPosition(1, end.transform.position);
+		if (Physics.Raycast(start.transform.position, dir, out hitInfo, Vector3.Distance(start.transform.position, end.transform.position)))
+		{
+			Debug.Log("we Hit this: " +  hitInfo.transform.GetComponent<ObjectDesc>().Name);
+			text.text = hitInfo.transform.GetComponent<ObjectDesc>().Name + ": " + hitInfo.transform.GetComponent<ObjectDesc>().Description;
+			Debug.DrawRay(start.transform.position, dir);
+			
+			currRend = hitInfo.collider.gameObject.GetComponent<Renderer>();
  
-            if (currRend == rend)
-                return;
+			if (currRend == rend)
+				return;
  
-            if (currRend && currRend != rend)
-            {
-                if (rend)
-                {
-                    rend.sharedMaterial = origMat;
-                }
+			if (currRend && currRend != rend)
+			{
+				if (rend)
+				{
+					rend.sharedMaterial = origMat;
+				}
  
-            }
+			}
  
-            if (currRend)
-                rend = currRend;
-            else
-                return;
+			if (currRend)
+				rend = currRend;
+			else
+				return;
  
-            origMat = rend.sharedMaterial;
+			origMat = rend.sharedMaterial;
 
-            tempMat = new Material(origMat);
-            rend.material = tempMat;
-            rend.material.shader = shader;
-        }
-        else
-        {
-            if (rend)
-            {
-                rend.sharedMaterial = origMat;
-                rend = null;
-                text.text = "";
-            }
-        }
-    }
+			tempMat = new Material(origMat);
+			rend.material = tempMat;
+			rend.material.shader = shader;
+		}
+		else
+		{
+			if (rend)
+			{
+				rend.sharedMaterial = origMat;
+				rend = null;
+				text.text = "";
+			}
+		}
+	}
 }
--- RadioTelescope/Assets/Scripts/Player/HighlightTarget.cs
+++ RadioTelescope/Assets/Scripts/Player/HighlightTarget.cs
@@ -5,79 +5,65 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
+// This script casts a ray when activated. Should this ray touch a part
+// of the radio telescope, that object is highlights, and the associated
+// ObjectDesc script's name and description is saved to be displayed to
+// the player.
 public class HighlightTarget : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject end;
-    public GameObject start;
-    private RaycastHit hitInfo;
-    private Material origMat, tempMat;
-    public Color highlightColor;
-    public Shader shader;
-    private Renderer rend = null;
-    private Renderer currRend;
-    public Text text;
-    void Start()
-    {
-        //origMat = rend.sharedMaterial;
-    }
+	// Start and end are two invisible cubes, one on top of the player
+	// and one off in the distance, between which the ray is cast.
+	public GameObject start;
+	public GameObject end;
+	public Color highlightColor;
+	public Shader shader;
+	public Text text;
+	
+	private RaycastHit hitInfo;
+	private Material origMat, tempMat;
+	private Renderer rend = null;
+	private Renderer currRend;
 
-    private void OnEnable()
-    {
-        //Debug.Log("Enabling HighlightTarget");
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		var dir = start.transform.forward * 10000;
+		if(Physics.Raycast(start.transform.position, dir, out hitInfo, Vector3.Distance(start.transform.position, end.transform.position)))
+		{
+			text.text = hitInfo.transform.GetComponent<ObjectDesc>().Name + ": " + hitInfo.transform.GetComponent<ObjectDesc>().Description;
+			Debug.DrawRay(start.transform.position, dir);
 
-    private void OnDisable()
-    {
-        rend.sharedMaterial = origMat;
-        //Debug.Log("Disabling HighlightTarget");
-        rend = null;
-        text.text = "";
-    }
+			currRend = hitInfo.collider.gameObject.GetComponent<Renderer>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        var dir = start.transform.forward * 10000;
-        if (Physics.Raycast(start.transform.position, dir, out hitInfo, Vector3.Distance(start.transform.position, end.transform.position)))
-        {
-            //Debug.Log("we Hit this: " + hitInfo.transform.ToString());
-            text.text = hitInfo.transform.GetComponent<ObjectDesc>().Name + ": " + hitInfo.transform.GetComponent<ObjectDesc>().Description;
-            Debug.DrawRay(start.transform.position, dir);
+			if(currRend == rend)
+				return;
 
-            currRend = hitInfo.collider.gameObject.GetComponent<Renderer>();
+			if(currRend && rend && currRend != rend)
+				rend.sharedMaterial = origMat;
 
-            if (currRend == rend)
-                return;
+			if(currRend)
+				rend = currRend;
+			else
+				return;
 
-            if (currRend && currRend != rend)
-            {
-                if (rend)
-                {
-                    rend.sharedMaterial = origMat;
-                }
-
-            }
-
-            if (currRend)
-                rend = currRend;
-            else
-                return;
-
-            origMat = rend.sharedMaterial;
-
-            tempMat = new Material(origMat);
-            rend.material = tempMat;
-            rend.material.shader = shader;
-        }
-        else
-        {
-            if (rend)
-            {
-                rend.sharedMaterial = origMat;
-                rend = null;
-                text.text = "";
-            }
-        }
-    }
+			origMat = rend.sharedMaterial;
+			tempMat = new Material(origMat);
+			rend.material = tempMat;
+			rend.material.shader = shader;
+		}
+		else if(rend)
+		{
+			rend.sharedMaterial = origMat;
+			rend = null;
+			text.text = "";
+		}
+	}
+	
+	private void OnDisable()
+	{
+		if(rend)
+			rend.sharedMaterial = origMat;
+		rend = null;
+		text.text = "";
+	}
 }
