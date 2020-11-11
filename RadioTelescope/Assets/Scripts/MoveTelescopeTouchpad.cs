af279d8d802534a7b3c8681ff8c65d0afcfc4c3d
YCPjsteck <jsteck@ycp.edu>
2020-11-11 12:56:53 -0500

    feat: The left controller touchpad now controls the telescope movement

---
+++ RadioTelescope/Assets/Scripts/MoveTelescopeTouchpad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.CameraRig.UnityXRCameraRig.Input;
+
+// A script to control the telescope via the left touchpad.
+public class MoveTelescopeTouchpad : MonoBehaviour
+{
+	public GameObject telescope;
+	public TelescopeController tc;
+	public UnityAxis1DAction leftTouchpadVertical;
+	public UnityAxis1DAction leftTouchpadHorizontal;
+
+	// Start is called before the first frame update.
+	void Start()
+	{
+		tc = telescope.GetComponent<TelescopeController>();
+	}
+
+	// Update is called once per frame.
+	void Update()
+	{
+		tc.RotateY(leftTouchpadHorizontal.Value);
+		tc.RotateZ(leftTouchpadVertical.Value);
+	}
+}
