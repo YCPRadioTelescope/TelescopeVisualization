34fb270dc1659657759abbb7adae7cbcc6f165fd
Derek Herr <dherr@ycp.edu>
2022-03-28 14:06:25 -0400

    Added correct descriptions of parts
    -all part descriptions are finished
    -added basic quest2 support for steam vr

--- RadioTelescope/Assets/Scripts/Sky_Spawner.cs
+++ RadioTelescope/Assets/Scripts/Sky_Spawner.cs
@@ -6,22 +6,50 @@ public class Sky_Spawner : MonoBehaviour
 {
     public GameObject sky_interaction_object;
     public GameObject star_system;
+	public TextAsset Sky_Data;
 
-    public float RA = 38;
-    public float DEC = 89;
 
-    private void Start()
+	private void Start()
     {
-        //PolarToCartesian(37.95f, 89.26f, 132)
-        //Vector3 position = new Vector3(1.3431f, 132, 1);
-        Vector3 position = PolarToCartesian(37.95f, 89.26f, 132);
-        position = Vector3.Normalize(position) * 900;
-        GameObject sky_interaction_clone = Instantiate(sky_interaction_object, position, Quaternion.identity);
-        sky_interaction_clone.gameObject.transform.SetParent(star_system.transform);
+		//Run the CSV only once at the start of the program, this creates all the interactable objects from the program
+		read_CSV();
     }
 
-    
-    Vector3 PolarToCartesian(float RA, float DEC, float D)
+	public void read_CSV()
+	{
+		// Split the Sky_Data file into each line.
+		string[] lines = Sky_Data.text.Split('\n');
+		// For each Line, create the interactable objects(Triangles)
+		for (int i = 0; i < lines.Length; i++)
+		{
+			// Split each line by the commas.
+			string[] components = lines[i].Split(',');
+
+			//Get the RA, DEC, Dist, and Label from the CSV for each line
+			float RA = float.Parse(components[0]);
+			float DEC = float.Parse(components[1]);
+			float Dist = float.Parse(components[2]);
+			//If the Distance is entered as 0, set to default of 500
+			if(Dist == 0)
+            {
+				Dist = 500f;
+            }
+			string label = components[3];
+			string image = components[4];
+			Debug.Log("Have just read data from " + label);
+
+			Vector3 position = PolarToCartesian(RA, DEC, Dist);
+			position = Vector3.Normalize(position) * 900;
+			GameObject sky_interaction_clone = Instantiate(sky_interaction_object, position, Quaternion.identity);
+			sky_interaction_clone.gameObject.transform.SetParent(star_system.transform);
+
+			string filepath = "Sky_Interaction_Data/" + image;
+			Texture2D tex = Resources.Load(filepath) as Texture2D;
+			Debug.Log("Loaded texture: " + tex);
+
+		}
+	}
+	Vector3 PolarToCartesian(float RA, float DEC, float D)
     {
         RA = RA * (Mathf.PI / 180);
         DEC = DEC * (Mathf.PI / 180);
