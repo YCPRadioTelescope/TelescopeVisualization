a7aa4eb67ff14767ecf1a9e6377174a030575d47
Derek Herr <dherr@ycp.edu>
2022-04-05 19:46:36 -0400

    Images can now be read from StreamedAssets
    -Shrek image can now be read from the streamedassets folder

--- RadioTelescope/Assets/Test_Load_Image.cs
+++ RadioTelescope/Assets/Test_Load_Image.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI;
 
 public class Test_Load_Image : MonoBehaviour
 {
-    private UnityEngine.UI.Image image = null;
-    private void Awake()
+    private Texture2D new_tex;
+    private void Start()
     {
-        if (image != null)
-        {
-            //image.mainTexture = Resources.Load<Image>("Images/test");
-        }
+        byte[] pngBytes = System.IO.File.ReadAllBytes(Application.streamingAssetsPath + "/Sky_Interaction_Data/Skrek.jpg");
+        Debug.Log(pngBytes);
+        new_tex = new Texture2D(128, 128);
+        new_tex.LoadImage(pngBytes);
+        transform.GetComponent<RawImage>().texture = new_tex;
     }
 }
